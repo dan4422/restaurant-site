1308ed56674c2bd1b21c1c29b11d5a9ca234dfe0
Brandon Drayton <brandondrayton@gmail.com>
2022-06-02 16:55:45 -0400

    menuRoutesadded_buttonConnected

--- routes/lunchMenu.js
+++ routes/lunchMenu.js
@@ -7,19 +7,20 @@ const models = require('../models')
 
 router.get('/', async (req, res) => {
     const products = await models.Product.findAll()
+    // const product = products.map((product => product.category))
+    const results = products.filter(p => p.category == 'Lunch')
     res.render("menuLayout", {
         partials: {
-            body: "partials/menu"
+            body: "partials/lunch-menu"
         },
         locals: {
-            title: "Welcome to not chick-fil-a menu!",
-            products
+            title: "Welcome to not chick-fil-a lunch menu!",
+            products,
+            results,
+
         }
     })
 })
-
-
-
 
 
 module.exports = router;
