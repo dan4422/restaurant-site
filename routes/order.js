320b57c7421690215819fb941ecc50931f6f74eb
Brice Tran <brice1992@msn.com>
2022-06-06 15:41:43 -0400

    receipt page

--- routes/order.js
+++ routes/order.js
@@ -26,7 +26,6 @@ router.get('/', async (req, res) => {
             include: models.Product
         }
     })
-    console.log(order.OrderProducts)
     if (!order.OrderProducts) {
         res.render("layout", {
             partials: {
@@ -49,8 +48,8 @@ router.get('/', async (req, res) => {
         }
     })
     }
-
 })
+
 router.post('/', (req, res) => {
     res.redirect('/order')
 })
@@ -114,7 +113,15 @@ router.post('/checkout', async (req,res) => {
 })
 
 router.get('/checkout/receipt', async (req, res) => {
-    const order = await models.Order.findOne({
+    const [user] = await models.User.findOrCreate({
+        where: { id: req.session.user?.id || null },
+        defaults: {
+            name: "guest"
+        }
+    })
+    req.session.user = user
+    loggedInUser = req.session.user
+    const [order] = await models.Order.findOrCreate({
         where: {
             status: 'in progress',
             UserId: req.session.user.id
@@ -124,16 +131,16 @@ router.get('/checkout/receipt', async (req, res) => {
             include: models.Product
         }
     })
-    res.render("layout", {
+        res.render("layout", {
         partials: {
             body: "partials/receipt"
         },
         locals: {
-            title: "Thank you!",
+            title: "Order Confirmation",
+            user: user,
+            order: order,
             orderProducts: order.OrderProducts,
-            user: order.Users,
             loggedInUser
-
         }
     })
 })
